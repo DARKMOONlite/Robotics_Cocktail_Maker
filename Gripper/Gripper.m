c63af889778432242fc3a0eeb9bdde403a2c900b
DARKMOONlite <sebastianschroder@hotmail.com>
2022-04-21 13:39:44 +1000

    PLy model for gripper & gripper class

---
+++ Gripper/Gripper.m
@@ -0,0 +1,23 @@
+classdef Gripper
+    %GRIPPER Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        Property1
+    end
+    
+    methods
+        function self = Gripper()
+            %GRIPPER Construct an instance of this class
+            %   Detailed explanation goes here
+            obj.Property1 = inputArg1 + inputArg2;
+        end
+        
+        function Create_Gripper(self,inputArg)
+            %METHOD1 Summary of this method goes here
+            %   Detailed explanation goes here
+            
+        end
+    end
+end
+
