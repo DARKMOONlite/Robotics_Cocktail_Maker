b8a471ef1a030efb466b8b067b2edc765c856994
DARKMOONlite <sebastianschroder@hotmail.com>
2022-05-04 16:07:03 +1000

    Added bounding box function for objects

--- Test_Script.m
+++ Test_Script.m
@@ -20,22 +20,43 @@ set(Axis_Control,'CameraViewAngleMode','Manual');
 hold on
 % robot = UR10
 % 
-g = Gripper;
+% g = Gripper;
 % 
 % UR10e =  UR10e();
-Objects_(1) = R_Object("Gin",10,10,[0,0,1],"Large");
+% Objects_(1) = R_Object("Gin",10,10,[0,0,1],"Large");
 % T = transl(0,0,2)
 % Objects_(1).move_object(T)
 
 % g = Gripper;
 % 
 % g.move_gripper(T)
-drawnow
-hold off
+% drawnow
+% hold off
 %matlab.graphics.primitive.Patch
 
 
 %  g.encompassing_grip(0.077)
+
+      m=0;
+      dx = 0.03;
+      corner_points = zeros(8,3);
+                for i =-1:2:1
+                    for j = -1:2:1
+                        for k = -1:2:1
+                            m = m+1;
+                            corner_points(m,:) = [k*(0.1/2+dx)+1, j*(0.1/2+dx)+1,i*(0.3+dx)+0];
+
+
+                        end
+
+                    end
+
+                end
+
+
+
+
+
 
 
 
