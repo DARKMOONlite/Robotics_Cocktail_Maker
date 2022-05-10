bcd08a4712011efce075ef50c62e60ba7dd02c63
DARKMOONlite <sebastianschroder@hotmail.com>
2022-05-10 11:53:36 +1000

    PourHandler

--- drinkHandler.m
+++ drinkHandler.m
@@ -6,26 +6,34 @@ function drinkHandler(code)
         action = codeArray(i);
         
         switch action
-            case '0'
+            case '0' % Serve Drink
                 disp('Drink action 0')
-            case '1'
+
+
+            case '1' % Pour Tonic
                 disp('Drink action 1')
-            case '2'
+            case '2' % Pour Gin
                 disp('Drink action 2')
-            case '3'
+            case '3' % Pour Rum
                 disp('Drink action 3')
-            case '4'
+            case '4' % Pour Vodka
                 disp('Drink action 4')
-            case '5'
+            case '5' % Add Sugar
                 disp('Drink action 5')
-            case '6'
+            case '6'% Add Lime
                 disp('Drink action 6')
-            case '7'
+            case '7'% Add Ice
                 disp('Drink action 7')
-            case '8'
+            case '8'% Shake
                 disp('Drink action 8')
-            case '9'
+            case '9' % Pour Shaker
                 disp('Drink action 9')
+
+            case 'a' % Change pour location to Glass
+                disp('Drink action a')
+
+            case 'b' %Change pour location to Shaker
+                disp('Drink action b')
             otherwise
                 disp('Error: Unassigned action code')
         end
